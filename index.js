f962f045bb10d1f4446d67bac7d73f4382b2a27f
unknown <79zabare812@gmail.com>
2026-08-15 13:32:16 +0300

    Initial website

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(a,b) {
+    return(a + b)
+}
+
+let result = add(13,9)
+console.log(result)
+
+
+function multiply(c,d) {
+    return(c + d)
+}
+
+let ans = multiply(12,9)
+console.log(ans)
+
+function name(x) {
+    return(x)
+}
+
+let remain = name("Hamza")
+console.log(remain)
